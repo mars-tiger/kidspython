92395c8cab17e5988b526c829485640fafe5d206
mars-tiger <51861316+mars-tiger@users.noreply.github.com>
2021-08-28 15:45:00 -0700

    Update al.py

--- tictactoe/al.py
+++ tictactoe/al.py
@@ -1,6 +1,19 @@
-import turtle
-
-t = turtle.Turtle()
-t.speed(10)
-
-t.circle(50)
+def draw_star(size, x, y):
+  t.up()
+  t.goto(x, y)
+  t.down()
+  t.forward(size)
+  t.backward(size/2)
+  t.left(60)
+  t.up()
+  t.forward(size/2)
+  t.down()
+  t.right(180)
+  t.forward(size)
+  t.left(180)
+  t.forward(size/2)
+  t.left(60)
+  t.up()
+  t.backward(size/2)
+  t.down()
+  t.forward(size)
