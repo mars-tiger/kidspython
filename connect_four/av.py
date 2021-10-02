a99f5607add4ecdbf00041a46b5e0d12f6dcbb70
Avapython <89699342+Avapython@users.noreply.github.com>
2021-10-02 12:56:04 -0700

    Update av.py

--- connect_four/av.py
+++ connect_four/av.py
@@ -1,16 +1,16 @@
 import turtle
-
-
-def draw_disk(color):
   
+def draw_piece(row, col, color):
+  x = offset_x + 25 + col * 2 * (radius + gap)
+  y = offset_y - 25 - row * 2 * (radius + gap)
+  t.up()
+  t.home()
+  t.goto(x,y)
+  t.down()
   t.color(color)
   t.begin_fill()
   t.circle(radius)
   t.end_fill()
-
-  
-def draw_piece(row, col, color):
-  pass
   
 def draw(x, y):
   global board, rb, winner
@@ -21,20 +21,29 @@ def check_winner():
   pass
 
 def draw_board():
-  pass
+ t.color("blue")
+ t.begin_fill()
+ t.up()
+ t.goto(190, -180)
+ t.down()
+ t.left(90)
+ t.forward(310)
+ t.left(90)
+ t.forward(380)
+ t.left(90)
+ t.forward(310)
+ t.left(90)
+ t.forward(380)
+ t.end_fill()
 
+   
+ 
 
-t.up()
-t.home()
-t.down()
-draw_disk("green")
-t.pu()
-t.goto(100,100)
-draw_disk("red")
-
-
-
-
+                 
+ for row in range(6):
+   for col in range(7):
+     draw_piece(row,col,"white")
+    
 radius = 23
 gap = 2
 square_size = 2 * (radius + gap)
@@ -59,6 +68,14 @@ t.ht()
 t.speed(200)
 
 draw_board()
+#draw_piece(0, 0, "blue")
+#draw_piece(0, 1, "red")
+#draw_piece(3, 5, "purple")
+
+t.up()
+t.home()
+t.down()
+
 
 wn = turtle.Screen()
 wn.onclick(draw)
