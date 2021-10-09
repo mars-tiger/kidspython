f4974b24c41562ef2f66dcf56c895873ffbc8357
mars-tiger <51861316+mars-tiger@users.noreply.github.com>
2021-10-09 13:45:06 -0700

    Update yh.py

--- connect_four/yh.py
+++ connect_four/yh.py
@@ -40,6 +40,14 @@ def check_winner():
     for col in range(4):
       if board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] and board[row][col]:
         return board[row][col]
+  for row in range(3):
+    for col in range(7):
+      if board [row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] and board[row][col]:
+        return board[row][col]
+  for row in range(3):
+    for col in range(4):
+      if board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] and board[row][col]:
+        return board[row][col]
   return ""
 
 def draw_board():
