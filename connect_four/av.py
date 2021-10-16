63d62d0083fb0c0904af6078916e0fe8ab2b9278
Avapython <89699342+Avapython@users.noreply.github.com>
2021-10-16 13:46:09 -0700

    Update av.py

--- connect_four/av.py
+++ connect_four/av.py
@@ -1,5 +1,6 @@
+
 import turtle
-  
+ 
 def draw_piece(row, col, color):
   x = offset_x + 25 + col * 2 * (radius + gap)
   y = offset_y - 25 - row * 2 * (radius + gap)
@@ -11,38 +12,40 @@ def draw_piece(row, col, color):
   t.begin_fill()
   t.circle(radius)
   t.end_fill()
+def draw_india():
   
+  t.up()
+  t.home()
+  t.goto(-150,150)
+  t.down()
+  t.color(rb)
+  t.begin_fill()
+  t.circle(20)
+  t.end_fill()
+ 
 def draw(x, y):
   global board, rb, winner
   col = int((x - offset_x) // square_size)
-  
+ 
   row = None
   for r in range(5, -1, -1):
     if not board[r][col]:
       row = r
       break
-  
+ 
   if row is None or winner:
     return
-  
+ 
   draw_piece(row, col, rb)
   board[row][col] = rb
-
-  winner = check_winner()
-  if winner:
-    t.pu()
-   
-    t.goto(0,140)
-    t.pd()
-    t.write("%s wins!" % rb, align="center", font=("Arial", 32, "bold"))
-    
+ 
   if rb == "red":
     rb = "blue"
   else:
     rb = "red"
  
-
-  
+  winner = check_winner()
+ 
 def check_winner():
   for row in range(6):
     for col in range(4):
@@ -54,6 +57,10 @@ def check_winner():
         return board[row][col]
   for row in range(3):
     for col in range(4):
+      if board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] and board[row][col]:
+        return board[row][col]
+  for row in range(3):
+    for col in range(3, 7):
       if board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] and board[row][col]:
         return board[row][col]
   return ""
@@ -78,7 +85,7 @@ def draw_board():
  for row in range(6):
    for col in range(7):
      draw_piece(row,col,"white")
-    
+   
 radius = 23
 gap = 2
 square_size = 2 * (radius + gap)
@@ -111,7 +118,7 @@ t.up()
 t.home()
 t.down()
 
-
+t.draw_india()
 wn = turtle.Screen()
 wn.onclick(draw)
 wn.mainloop()
